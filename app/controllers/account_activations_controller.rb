49f980a09ce5e2349853f8a4ec9ecb8130933fd2
dattx1 <tran.xuan.dat@framgia.com>
2017-01-10 16:12:46 +0700

    fix error

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,4 +1,4 @@
-class AccountActivationsController < ApplicationControllerAccount
+class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by email: params[:email]
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
