778dd9b971b6d4c44d7ad6e8525a9afe9fc740b4
dattx1 <tran.xuan.dat@framgia.com>
2017-01-10 19:07:49 +0700

    update send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,11 +62,11 @@ Rails.application.configure do
   host = "https://dattx1-sample-app.herokuapp.com"
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
-    address:        "smtp.sendgrid.net",
+    address:        "smtp.gmail.com",
     port:            "587",
     authentication:  :plain,
-    user_name:       ENV["SENDGRID_USERNAME"],
-    password:        ENV["SENDGRID_PASSWORD"],
+    user_name:       "dattx5@gmail.com",
+    password:        "dargonrong1",
     domain:          "heroku.com",
     enable_starttls_auto: true
   }
