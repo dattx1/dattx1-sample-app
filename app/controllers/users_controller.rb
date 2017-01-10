151483d7dc140c5da956cc54bd4877648674e48a
dattx1 <tran.xuan.dat@framgia.com>
2017-01-10 18:23:59 +0700

    update active mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = edit_account_activation_url(@user.activation_token,  email: @user.email)
+      #flash[:success] = edit_account_activation_url(@user.activation_token,  email: @user.email)
+      @user.send_activation_email
       flash[:info] = t "message_activation_email"
       redirect_to root_url
     else
